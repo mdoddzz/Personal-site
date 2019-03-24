c0908bc1f0d744a461672ab57e31c8522220ebd5
Mike Dodd <mike@wilxite.com>
2019-03-24 10:57:46 +0000

    Added font awesome pro

--- widgets/contact.php
+++ widgets/contact.php
@@ -1,2 +1,26 @@
 <h1>Contact.</h1>
 <h3>If for any reason you would like to contact me<br>I guess here is the best place to do so</h3>
+
+<div class="employmentStatus">
+    <strong>Employment Status: </strong> Happily employed @ Wilxite Ltd
+</div>
+
+<div class="containerSplit thirdContainer reverseContainer noOutsideMargin">
+
+    <div class="container">
+
+        <p><strong>Email:</strong> contact@michaeldodd.co.uk</p>
+        <p><strong>Twitter:</strong> @mdoddzz</p>
+        <p><strong>Linkedin:</strong> linkedin.com/afdsfsd</p>
+
+    </div>
+
+    <div class="container twoThirds">
+
+        <div class="formContainer">
+
+        </div>
+
+    </div>
+
+</div>
