ff20da3777420d85af61f6df675596d8400bcf82
Mike Dodd <mike@wilxite.com>
2019-08-04 21:56:41 +0100

    multiplied dummy portfolio projects

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -8,26 +8,80 @@ A collection of what I spend my free time doing
 
 @section('content')
 
-<div class="portfolioContainer">
+<div class="portfolioList">
 
-    <img class="portfolioImg" src="https://countrylakesdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.jpg" />
+    <div class="portfolioContainer">
 
-    <div>
+        <img class="portfolioImg" src="https://countrylakesdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.jpg" />
 
-        <h4>Portfolio item title</h4>
+        <div>
 
-        <p>Short description of the project goes here saying what it does</p>
+            <h4>Portfolio item title</h4>
 
-        <ul class="portfolioTags">
-            <li>PHP</li>
-            <li>mySQL</li>
-            <li>JQuery</li>
-        </ul>
+            <p>Short description of the project goes here saying what it does</p>
 
-        <div class="clearfix"></div>
+            <ul class="portfolioTags">
+                <li>PHP</li>
+                <li>mySQL</li>
+                <li>JQuery</li>
+            </ul>
 
-        <a class="button">View Details</a>
-        <a class="button">View Project</a>
+            <div class="clearfix"></div>
+
+            <a class="button">View Details</a>
+            <a class="button">View Project</a>
+
+        </div>
+
+    </div>
+
+    <div class="portfolioContainer">
+
+        <img class="portfolioImg" src="https://countrylakesdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.jpg" />
+
+        <div>
+
+            <h4>Portfolio item title</h4>
+
+            <p>Short description of the project goes here saying what it does</p>
+
+            <ul class="portfolioTags">
+                <li>PHP</li>
+                <li>mySQL</li>
+                <li>JQuery</li>
+            </ul>
+
+            <div class="clearfix"></div>
+
+            <a class="button">View Details</a>
+            <a class="button">View Project</a>
+
+        </div>
+
+    </div>
+
+    <div class="portfolioContainer">
+
+        <img class="portfolioImg" src="https://countrylakesdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.jpg" />
+
+        <div>
+
+            <h4>Portfolio item title</h4>
+
+            <p>Short description of the project goes here saying what it does</p>
+
+            <ul class="portfolioTags">
+                <li>PHP</li>
+                <li>mySQL</li>
+                <li>JQuery</li>
+            </ul>
+
+            <div class="clearfix"></div>
+
+            <a class="button">View Details</a>
+            <a class="button">View Project</a>
+
+        </div>
 
     </div>
 
