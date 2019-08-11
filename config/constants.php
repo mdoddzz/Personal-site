241efae69a8585ab396952f34d09d234d4937920
Mike Dodd <mike@wilxite.com>
2019-08-11 21:45:21 +0100

    Added constants and started dynamic home page

---
+++ config/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'app_colours' => [
+        'green', 
+        'blue', 
+        'pink'
+    ],
+
+    'home_shapes' => [
+        '',
+        'twoThirds'
+    ]
+
+];
+
+?>
